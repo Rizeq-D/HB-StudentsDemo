b605a176f23cefa65e509d407082bb9b751e16b6
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2023-12-06 13:17:29 +0100

    had to edit the code

---
+++ src/main/java/com/luv2code/hibernate/demo/CreateStudentImagesSetDemo.java
@@ -0,0 +1,72 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+public class CreateStudentImagesSetDemo {
+
+    public static void main( String[] args ) {
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // Create the object
+            Student tempStudent = new Student("Rami", "Malek", "rami@gmail.com");
+            Set<String> theImages = tempStudent.getImages();
+
+            theImages.add("photo1.jpg");
+            theImages.add("photo2.jpg");
+            theImages.add("photo3.jpg");
+            theImages.add("photo4.jpg");
+
+            // start the transaction
+            session.beginTransaction();
+            System.out.println("saving the student and the images");
+            session.persist(tempStudent);
+
+            // commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+        }
+
+        // clean up the code
+        finally {
+            session.close();
+            factory.close();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
